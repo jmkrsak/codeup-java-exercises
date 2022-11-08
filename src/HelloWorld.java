543f0d6abb47f68f8b741fc3b5844301f8f4c75b
John-Michael Krsak <jmkrsak@gmail.com>
2022-11-08 09:59:39 -0500

    java exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,44 @@ public class HelloWorld {
         public static void main(String[] args) {
             System.out.println("Hello, World!");
             System.out.print("Hello, ");
-            System.out.print("World!");
+            System.out.print("World!\n");
+            int myFavoriteNumber = 16;
+            System.out.print(myFavoriteNumber + "\n");
+//            String myString = "CodeUp";
+//            System.out.println(myString);
+            char myString = 'C';
+            System.out.println(myString);
+//            String myString = "3.14159";
+//            System.out.println(myString);
+//            double myNumber = 3.14;
+//            System.out.println(myNumber);
+//            float myNumber = 123L;
+//            System.out.println(myNumber);
+            float myNumber = 123;
+            System.out.println(myNumber);
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+//            int x = 5;
+//            System.out.println(++x);
+//            System.out.println(x);
+//            String class = "yo";
+//            String theNumberThree = "three";
+//            Object o = theNumberThree;
+//            int three = (int) o;
+//            System.out.println(three);
+//            int x = 4;
+//            x = x + 5;
+//            x += 5;
+//            System.out.print(x);
+//            int x = 3;
+//            int y = 4;
+//            y *= x;
+//            System.out.print(y);
+            int x = 10;
+            int y = 2;
+            x /= y;
+            y -= x;
+            System.out.println(x + ", " + y);
         }
 }
