c891a1e822c3d7a2a6e09f663d9ecda41bae04e9
John-Michael Krsak <jmkrsak@gmail.com>
2022-11-13 14:46:31 -0500

    bob exercise complete

---
+++ src/Bob.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Bob {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        boolean talkToTeen = true;
+
+        while (talkToTeen) {
+
+            System.out.print("What would you like to say to Bob?: ");
+            String input = scanner.nextLine();
+
+            if (input.endsWith("?") == true) {
+                System.out.println("Sure.");
+            } else if (input.endsWith("!") == true) {
+                System.out.println("Whoa, chill out!");
+            } else if (input.trim().length() == 0) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+
+            System.out.print("Would you like to talk to Bob? [y / n]: ");
+            String answer = scanner.nextLine();
+
+            if (answer.equalsIgnoreCase("y")) {
+                talkToTeen = true;
+            } else {
+                talkToTeen = false;
+            }
+
+        }
+
+    }
+
+}
