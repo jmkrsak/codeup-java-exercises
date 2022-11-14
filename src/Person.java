41b87bb8783b5b805851e212c56274521f6c07e5
John-Michael Krsak <jmkrsak@gmail.com>
2022-11-14 15:51:07 -0500

    update

---
+++ src/Person.java
@@ -0,0 +1,33 @@
+public class Person {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void sayHello(){
+        System.out.println("Hello " + getName());
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setName("Michael");
+        System.out.println(person.getName());
+        person.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+    }
+
+}
