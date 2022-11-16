1f47c282f34c62dd08b8984787d461001c8a8259
John-Michael Krsak <jmkrsak@gmail.com>
2022-11-16 01:47:01 -0500

    refactored

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -31,67 +31,70 @@ public class MoviesApplication {
         System.out.printf("%s\n",movie.getCategory());
     }
     private static void drawMovies(Movie[] allMovies, String selectedCategory) {
-        if(selectedCategory.equals("all")) {
-            for(Movie movie: allMovies) {
+        if (selectedCategory.equals("all")) {
+            for (Movie movie : allMovies) {
                 drawMovie(movie);
 
             }
         } else {
-        for(Movie movie: allMovies) {
-            if(movie.getCategory().equalsIgnoreCase(selectedCategory)) {
-                drawMovie(movie);
+            for (Movie movie : allMovies) {
+                if (movie.getCategory().equalsIgnoreCase(selectedCategory)) {
+                    drawMovie(movie);
+                }
             }
         }
     }
-    public static void main(String[] args) {
+        public static void main (String[]args){
 
-        Scanner scan = new Scanner(System.in);
-        MoviesApplication moviesApplication = new MoviesApplication;
-        boolean exitConditionMet = false;
+            Scanner scan = new Scanner(System.in);
+            MoviesApplication moviesApplication = new MoviesApplication(scan);
+            boolean exitConditionMet = false;
 
-        while(!exitConditionMet) {
-            System.out.println("0 - Exit");
-            System.out.println("1 - Add movie");
-            System.out.println("2 - View all movies");
-            for (int i = 3; i < moviesApplication.categories.length + 3; i++) {
-                System.out.printf("%d - view movies in the %s category\n", i, moviesApplication.categories[i - 3]);
-            }
+            while (!exitConditionMet) {
+                System.out.println("0 - Exit");
+                System.out.println("1 - Add movie");
+                System.out.println("2 - View all movies");
+                for (int i = 3; i < moviesApplication.categories.length + 3; i++) {
+                    System.out.printf("%d - view movies in the %s category\n", i, moviesApplication.categories[i - 3]);
+                }
 
 
-            Input input = new Input(new Scanner(System.in));
-            System.out.println();
-            int choice = input.getInt("Enter your choice: ".describeConstable());
-            if (choice != 0 && choice != 1 && choice != 2) {
-                String selectedCategory = moviesApplication.categories[choice - 3];
+                Input input = new Input(new Scanner(System.in));
                 System.out.println();
-                drawMovies(allMovies, selectedCategory);
-            } else if (choice == 0) {
-                exitConditionMet = true;
-            } else if (choice == 1) {
-                moviesApplication.addMovieFlow();
-            } else if (choice == 2) {
-                drawMovies(allMovies, "all");
-            } else {
-                System.out.println("Invalid choice, please select a valid choice");
+                int choice = input.getInt("Enter your choice: ");
+                Movie[] allMovies = moviesApplication.moviesArray.findAll();
+                if (choice != 0 && choice != 1 && choice != 2) {
+                    String selectedCategory = moviesApplication.categories[choice - 3];
+                    System.out.println();
+                    drawMovies(allMovies, selectedCategory);
+                } else if (choice == 0) {
+                    exitConditionMet = true;
+                } else if (choice == 1) {
+                    moviesApplication.addMovieFlow();
+                } else if (choice == 2) {
+                    drawMovies(allMovies, "all");
+                } else {
+                    System.out.println("Invalid choice, please select a valid choice");
+                }
+
             }
 
+
         }
 
-
-    }
-
-    private void addMovieFlow(){
-        System.out.print("Enter the name of the new movie: ");
-        String movieName = this.scanner.nextLine();
-        for(int i = 1; i < categories.length + 1; i++) {
-            System.out.printf("%d - %s\n",i,categories[i - 1]);
+        private void addMovieFlow () {
+            System.out.print("Enter the name of the new movie: ");
+            String movieName = this.scanner.nextLine();
+            for (int i = 1; i < categories.length + 1; i++) {
+                System.out.printf("%d - %s\n", i, categories[i - 1]);
+            }
+            System.out.println();
+            System.out.print("Choose the category: ");
+            int selectedCategory = this.scanner.nextInt() - 1;
+            this.scanner.nextLine();
+            System.out.println();
+            System.out.printf("New Movie Name: %s \nNew Movie Category: %s", movieName, this.categories[selectedCategory]);
+            this.moviesArray.addMovie(new Movie(movieName,this.categories[selectedCategory]));
         }
-        System.out.println();
-        System.out.print("Choose the category: ");
-        int selectedCategory = this.scanner.nextInt() - 1;
-        System.out.println();
-        System.out.printf("New Movie Name: %s \nNew Movie Category: %s",movieName,this.categories[selectedCategory]);
-        this.moviesArray.addMovie(new Movie(movieName.this.categories[selectedCategory]));
     }
 
-}
