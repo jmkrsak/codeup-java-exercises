d20953e76dbb5efcfce4e299fcc5d97388aa4861
John-Michael Krsak <jmkrsak@gmail.com>
2022-11-18 02:07:44 -0500

    update

--- src/adventureGame/Adventure.java
+++ src/adventureGame/Adventure.java
@@ -3,6 +3,57 @@ package adventureGame;
 public class Adventure {
 
     public static void main(String[] args) {
+        System.out.println("                                    ░░░░░░░░░░░░░░░░░░░░░░░░        \n" +
+                "                             ░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░░░    \n" +
+                "                           ░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░░  \n" +
+                "                           ░░▒▒▒▒▒▒▒▒▒▒▒▒████████████████▒▒▒▒░░  \n" +
+                "                         ░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒██            ██▒▒▒▒▒▒░░\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒▒▒▒▒██            ██▓▓▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒▒▒▒▒██          ██▓▓▒▒▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒▒▒██          ██▓▓▒▒▒▒▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒▒▒██        ████████▒▒▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒██                ██▒▒▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒████████        ██▓▓▒▒▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓██      ██▓▓▒▒▒▒▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒▒▒▒▒██      ██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒▒▒▒▒██    ██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒▒▒██    ██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n" +
+                "                         ▒▒▒▒▒▒▒▒▒▒▒▒██  ██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n" +
+                "                           ▒▒▒▒▒▒▒▒██  ██▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  \n" +
+                "                           ▒▒▒▒▒▒▒▒████▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  \n" +
+                "                             ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒    \n" +
+                "                                 ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒        \n");
+        System.out.println("\n" +
+                "████████ ██   ██ ██    ██ ███    ██ ██████  ███████ ██████       ██████  ██    ██ ███    ██ ██ \n" +
+                "   ██    ██   ██ ██    ██ ████   ██ ██   ██ ██      ██   ██     ██       ██    ██ ████   ██ ██ \n" +
+                "   ██    ███████ ██    ██ ██ ██  ██ ██   ██ █████   ██████      ██   ███ ██    ██ ██ ██  ██ ██ \n" +
+                "   ██    ██   ██ ██    ██ ██  ██ ██ ██   ██ ██      ██   ██     ██    ██ ██    ██ ██  ██ ██    \n" +
+                "   ██    ██   ██  ██████  ██   ████ ██████  ███████ ██   ██      ██████   ██████  ██   ████ ██ \n" +
+                "                                                                                               \n" +
+                "                                                                                               \n");
+        System.out.println("                                   ___\n" +
+                "                                 ./  _)                                               O\n" +
+                "                          _.---._|____\\__________________,-------.------------==-.____|]\n" +
+                " ____                    /  ##-|=======--------===-----==-|-----=|=-------------._______)\n" +
+                "(####L-.________________/  /___|__________________________ ______|               )-------.\n" +
+                " \\###|  ________________) `--- |---------------------===== ,_____|         _____/========\"\n" +
+                "  |##F-'              | /\\ --- |         __mmmmm__________(======|...---''/\n" +
+                "  |###/               (_\\ \\    |_______ ' \"-L  nn [mf]    |      |\n" +
+                "  |##|                     \"\"\"\"        \\__________________|      |\n" +
+                " /##/                                   )       //((  // ||   _,-/\n" +
+                "/##/                                   /       /\\\\_V_//   |,-\"  (\n" +
+                "\\#/                                  ./       /  `---\"     \\     \\\n" +
+                " v                                  /_       /              \\     `.\n" +
+                "                                   /_ `-.___/                \\      `.\n" +
+                "                                     `-.___/                  \".      `.\n" +
+                "                                                                \".      `.\n" +
+                "                                                                  \".    _,\"\n" +
+                "                                                                    \".-\"\n" +
+                "\n" +
+                "------------------------------------------------\n" +
+                "Thank you for visiting https://asciiart.website/\n" +
+                "This ASCII pic can be found at\n" +
+                "https://asciiart.website/index.php?art=objects/guns\n");
         //Thunder Gun
         System.out.println("Thunder Gun is a warrior from a post apocalyptic underground society,\nwho has to save his lover from an evil robot army!");
         //Thunder Gun moto
@@ -92,7 +143,115 @@ public class Adventure {
         //Dee
         System.out.println("What have I done?");
         //Charlie
-        System.out.println("You know what I'm going to hold my cloths over my head because that \"wall\" is coming soon.\nIf my cloths are up all that \"stuff\" doesn't get on your cloths, and it's more refreshing than you'd think.\nPlus ");
+        System.out.println("You know what I'm going to hold my cloths over my head because that \"wall\" is coming soon.\nIf my cloths are up all that \"stuff\" doesn't get on our cloths, and it's more refreshing than you'd think.\nPlus you have dry cloths after... better than having the sewage on your cloths and you the rest of the day.");
+        //Dee
+        System.out.println("I'm going to keep my cloths on");
+        //Event
+        System.out.println("Charlie and Dee continue walking through the sewer and are about to cross exposed pipes.");
+        //Dee
+        System.out.println("... I'M STUCK!!");
+        //Charlie
+        System.out.println("Pull it out!");
+        //Dee
+        System.out.println("I can't Charlie! We're going to miss the movie!");
+        //Charlie
+        System.out.println("You're right... I'll come back for you...");
+        //Dee
+        System.out.println("NO NO NO! DON'T DO THAT!");
+        //Event
+        System.out.println("The sound of rushing water is echoed through the sewer traveling quickly towards them.");
+        //Dee
+        System.out.println("Whats that sound? Is that the wall of \"water\"?");
+        //Event
+        System.out.println("Denis and his rear and the woman of his dreams whom he rear ended are now sitting in his car.\nHe won her favor by telling her by telling her he owned a chain of auto body shops,\nand that he'd take care of her car for free.");
+        //Denis
+        System.out.println("Yeah, my guys are already send a tow truck as we speak. I'm going to fix it up good as new, don't worry.");
+        //Girl
+        System.out.println("It's cool you own your own auto body repair shop.");
+        //Denis
+        System.out.println("Well you know auto body repair has always been a passion of mine...");
+        //Event
+        System.out.println("Out of no where Mac jumps in Denis's car. Denis is visibly upset.");
+        //Mac
+        System.out.println("No man left behind!");
+        //Event
+        System.out.println("Frank has won over the tourist on the boat and is giving an instructional class on how to navigate\nUS sewer systems.");
+        System.out.println("Sometimes you want to hold your cloths above the coming wall of \"water\".\nI also played a troll in \"The Nightman Cometh\". \"You got to pay the troll toll, to get into that hole!\"\nBut everyone misunderstood what I was saying.");
+        //Event
+        System.out.println("Frnak then jumps off the boat as it passes a nearby dock. The people on the boat yell towards him,\n\"But you're our tour guide!\"");
+        //Frank
+        System.out.println("Welcome to Philly");
+        //Event
+        System.out.println("Frank shouted while running off the dock away from the boat.");
+        //Frank
+        System.out.println("No hesitation! No surrender!.. No surrender, no surrender, no surrender, no surrender...");
+        //Event
+        System.out.println("As Frank was running away from the dock towards the nearest road, a squad of police were standing by to receive him.");
+        //Frank
+        System.out.println("I surrender!");
+        //Event
+        System.out.println("We're now inside the theater, in a dark screening room\nwho's only light is being emitted by the movies projector screen...\nThe audience is completely silent excluding the sound of someone aggressively opening candy.");
+        //Event
+        System.out.println("Charlie walks into the theater and see's Mac eating candy.");
+        //Charlie
+        System.out.println("Hey Mac you made it?");
+        //Mac
+        System.out.println("Even got the best seat, where's Dee?");
+        //Charlie
+        System.out.println("She couldn't make it; not \"Top Gun\" enough! Where's Denis?");
+        //Mac
+        System.out.println("Int the back of the theater going \"Thunder Gun\" with some girl he met.");
+        //Event
+        System.out.println("Dee walks into the theater covered in sewage... But her cloths are dry?");
+        //Dee
+        System.out.println("My cloths are dry Charlie. Your trick worked.");
+        //Charlie
+        System.out.println("That \"wall\" came right?");
+        //Dee
+        System.out.println("It came with a vengeance!");
+        //Charlie
+        System.out.println("I wonder where frank is?");
+        //Event
+        System.out.println("Frank is sitting with handcuffs on in an intake cell of the local police station.");
+        //Police
+        System.out.println("You have one phone call make it quick... Frank pick up the phone and begins to dial...");
+        //Event
+        System.out.println("An alarm is sounded, strobe lights replace the movie screen illuminating the theater.");
+        //usher
+        System.out.println("Everyone has to get out, there's been a BOMB THREAT!");
+        //Denis
+        System.out.println("Bomb threat?");
+        //Charlie
+        System.out.println("No way!");
+        //Denis
+        System.out.println("He \"Thunder Gunned\" us!");
+        //Charlie
+        System.out.println("He \"Thunder Gunned\" all of us.");
+        //Denis-Mac-Charlie
+        System.out.println("FRRAAAAANK!");
+        //Frank
+        System.out.println("NO hesitation, No surrender!");
+        System.out.println("\n" +
+                "███    ██  ██████      ██   ██ ███████ ███████ ██ ████████  █████  ████████ ██  ██████  ███    ██    \n" +
+                "████   ██ ██    ██     ██   ██ ██      ██      ██    ██    ██   ██    ██    ██ ██    ██ ████   ██    \n" +
+                "██ ██  ██ ██    ██     ███████ █████   ███████ ██    ██    ███████    ██    ██ ██    ██ ██ ██  ██    \n" +
+                "██  ██ ██ ██    ██     ██   ██ ██           ██ ██    ██    ██   ██    ██    ██ ██    ██ ██  ██ ██    \n" +
+                "██   ████  ██████      ██   ██ ███████ ███████ ██    ██    ██   ██    ██    ██  ██████  ██   ████ ▄█ \n" +
+                "                                                                                                     \n" +
+                "                                                                                                     \n" +
+                "███    ██  ██████      ███████ ██    ██ ██████  ██████  ███████ ███    ██ ██████  ███████ ██████  ██ \n" +
+                "████   ██ ██    ██     ██      ██    ██ ██   ██ ██   ██ ██      ████   ██ ██   ██ ██      ██   ██ ██ \n" +
+                "██ ██  ██ ██    ██     ███████ ██    ██ ██████  ██████  █████   ██ ██  ██ ██   ██ █████   ██████  ██ \n" +
+                "██  ██ ██ ██    ██          ██ ██    ██ ██   ██ ██   ██ ██      ██  ██ ██ ██   ██ ██      ██   ██    \n" +
+                "██   ████  ██████      ███████  ██████  ██   ██ ██   ██ ███████ ██   ████ ██████  ███████ ██   ██ ██ \n" +
+                "                                                                                                     \n" +
+                "                                                                                                     \n");
+
+
+
+
+
+
 
 
 
