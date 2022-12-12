33132184940560cdfd897dc925d8823e6227562e
John-Michael Krsak <jmkrsak@gmail.com>
2022-12-11 21:33:52 -0500

    update

--- src/grades/Grocery.java
+++ src/grades/Grocery.java
@@ -15,57 +15,83 @@ public class Grocery {
     private static boolean yesNoInput;
 
     public static HashMap<String, ArrayList<String>> getCompleteGroceryList() {
+
         return completeGroceryList;
+
     }
 
     public static void setCompleteGroceryList(HashMap<String, ArrayList<String>> completeGroceryList) {
+
         Grocery.completeGroceryList = completeGroceryList;
+
     }
 
     public static String getGroceryListName() {
+
         return groceryListName;
+
     }
 
     public static void setGroceryListName(String groceryListName) {
+
         Grocery.groceryListName = groceryListName;
+
     }
 
     public static ArrayList<String> getGroceryList() {
+
         return groceryList;
+
     }
 
     public static void setGroceryList(ArrayList<String> groceryList) {
+
         Grocery.groceryList = groceryList;
+
     }
 
     public static String getItemCategory() {
+
         return itemCategory;
+
     }
 
     public static void setItemCategory(String itemCategory) {
+
         Grocery.itemCategory = itemCategory;
+
     }
 
     public static String getItemName() {
+
         return itemName;
+
     }
 
     public static void setItemName(String itemName) {
+
         Grocery.itemName = itemName;
+
     }
 
     public static String getNumOfItems() {
+
         return numOfItems;
+
     }
 
     public static void setNumOfItems(String numOfItems) {
+
         Grocery.numOfItems = numOfItems;
+
     }
 
     private static HashMap<String, ArrayList<String>> completeGroceryList = new HashMap<>();
 
     public static void makeGroceryHash() {
+
         completeGroceryList.put(groceryListName, groceryList);
+
     }
 
     private static String groceryListName;
@@ -85,26 +111,25 @@ public class Grocery {
     private static String numOfItems;
 
     public Grocery(HashMap<String, ArrayList<String>> completeGroceryList) {
+
         this.completeGroceryList = completeGroceryList;
+
     }
 
     public static void groceryApp3() {
 
         itemCategory = input.getString("Select a category:\nBeverages | Bread | Canned | Dairy | Meat | Produce | Other: ");
-
         itemName = input.getString("Enter the name of the item: ");
-
         numOfItems = input.getString("Enter the number of items: ");
-
         yesNoInput = input.yesNo("Would you like to finalize this list? If you select no, you can make a new item.\n(Y)es or (N)o: ");
 
         if (yesNoInput) {
 
-
-
             makeArrayList();
             makeGroceryHash();
+
             System.out.println(completeGroceryList.get(groceryListName));
+
             for(String i: completeGroceryList.keySet()) {
                 System.out.print(completeGroceryList.get(i));
             }
@@ -122,7 +147,6 @@ public class Grocery {
     public static void groceryApp2() {
 
         groceryListName = input.getString("Enter a title for your grocery list: ");
-
         yesNoInput = input.yesNo("Would you like to enter a new item? (Y)es or (N)o: ");
 
         if (yesNoInput) {
@@ -133,6 +157,7 @@ public class Grocery {
 
         makeArrayList();
         makeGroceryHash();
+
         System.out.println("Okay, goodbye");
 
         }
